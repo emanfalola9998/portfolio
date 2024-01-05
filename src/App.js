93fbf0a62646ff185657a6082e1e66eb53bd82ec
emmanuelfalola <emmanuelfalola9998u@gmail.com>
2024-01-05 20:17:33 +0000

    removed download buttons

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
                 <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Emmanuel Falola</h1>
                 <h3 className="display-8  mb-3">Full Stack Developer</h3>
 
-                <div className="d-grid gap-2 d-md-flex justify-content-md-start mt-4">
+                {/* <div className="d-grid gap-2 d-md-flex justify-content-md-start mt-4">
                   <button type="button" className="btn btn-light btn-lg px-4 me-md-2"><svg xmlns="http://www.w3.org/2000/svg"
                     width="16" height="16" fill="currentColor" className="bi bi-apple mb-1" viewBox="0 0 16 16">
                     <path
@@ -31,7 +31,7 @@ function App() {
                     <path
                       d="M14.222 9.374c1.037-.61 1.037------------2.137 0-2.748L11.528 5.04 8.32 8l3.207 2.96 2.694-1.586Zm-3.595 2.116L7.583 8.68 1.03 14.73c.201 1.029 1.36 1.61 2.303 1.055l7.294-4.295ZM1 13.396V2.603L6.846 8 1 13.396ZM1.03 1.27l6.553 6.05 3.044-2.81L3.333.215C2.39-.341 1.231.24 1.03 1.27Z" />
                   </svg> Download</button>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
